e428b62495612f9bff6eeeb788103a59a85aa689
cprybell <cprybell@sbcglobal.net>
2019-09-24 21:40:12 -0700

    Got the questions and potential answers to display on the html webpage. Working on how to handle submissions of the answers now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@ var incorrectAnswers = 0;
 var totalQuestions = 20;
 var timeLeft = 100;
 var intervalId;
+var answerStackCounter = 0;
 
 function countDown() {
     timeLeft -= 1;
@@ -19,3 +20,54 @@ function gameOver() {
     console.log('game Over');
 }
 
+function QuestionObject(question, answers, answer) {
+    this.questionString = question;
+    this.potentialAnswers = answers;
+    this.correctAnswer = answer;
+  }
+
+  var questions = {
+      questionOne : new QuestionObject("Who won the Ballon D'or (best male soccer player) in the year 2012", ['Lionel Messi', 'Cristiano Ronaldo', 'Neymar Jr'], "Lionel Messi"),
+      questionTwo : new QuestionObject("Which team won the very first super bowl in 1967", ['Chiefs', 'Cowboys', 'Packers', 'Steelers'], "Packers"),
+  }
+
+  console.log(questions.questionOne);
+  console.log(questions.length);
+
+  function createQuestionHTML(objectQuestion) {
+    var newQuestionDiv = $("<div>");
+    var triviaDiv = $("<div>");
+    triviaDiv.text(objectQuestion.questionString);
+    newQuestionDiv.append(triviaDiv);
+    var answersDiv = $("<div>");
+    for (i = 0; i < objectQuestion.potentialAnswers.length; i++) {
+        createCheckBox(answersDiv, i, objectQuestion.potentialAnswers[i]);
+    }
+    newQuestionDiv.append(answersDiv);
+    $("#questionSpace").append(newQuestionDiv);
+    answerStackCounter++;
+  }
+
+  function createCheckBox(divToAddTo, count, value) {
+      var tempDiv = $("<div>");
+      tempDiv.addClass("form-check form-check-inline");
+      var tempInput = $("<input>");
+      tempInput.addClass("form-check-input inlineRadioOptions" + answerStackCounter);
+      tempInput.attr('type','radio');
+      tempInput.attr('name','inlineRadioOptions' + answerStackCounter);
+      tempInput.attr('id', "inlineRadio" + count);
+      tempInput.attr('value', value);
+      var tempLabel = $("<label>");
+      tempLabel.addClass("form-check-label");
+      tempLabel.attr('for', "inlineRadio" + count);
+      tempLabel.text(value);
+      tempDiv.append(tempInput);
+      tempDiv.append(tempLabel);
+      divToAddTo.append(tempDiv);
+  }
+
+  for (que in questions) {
+      createQuestionHTML(questions[que]);
+  }
+
+  //$(".inlineRadioOptions0").attr('disabled', true);
